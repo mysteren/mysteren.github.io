2f91d7d5be1c45d766d5a6f2c9bb8c00a5eff72c
Timofey <mysteren.moon@gmail.com>
2019-06-03 15:34:52 +0300

    fix fade in animation, refactoring

--- skyeng-test-task/main.js
+++ skyeng-test-task/main.js
@@ -5,26 +5,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // открыть модльник подтверждения
     function openConfirmModal() {
-        modalsBlock.classList.remove('modals--hidden', 'modals--fadeout');
+        modalsBlock.classList.remove('modals--fadeout');
     }
 
     // закрыть модальник подтверждения
     function closeConfirmModal() {
         modalsBlock.classList.add('modals--fadeout');
-        setTimeout(function() {
-            modalsBlock.classList.add('modals--hidden');
-        }, 300);
     }
 
     // обработка нажатия на оверлей и кнопку закрытия в модальном окне
-    Array.from(modalsBlock.querySelectorAll('.overlay, .confirm-modal__close-button')).forEach(elem => {
+    modalsBlock.querySelectorAll('.overlay, .confirm-modal__close-button').forEach(elem => {
         elem.addEventListener('click', function(event) {
             closeConfirmModal();
         });
     });
 
     // обработка нажатия на элемент списка
-    Array.from(document.querySelectorAll('.block-devices__list .block-devices__elem')).forEach(elem => {
+    document.querySelectorAll('.block-devices__list .block-devices__elem').forEach(elem => {
         elem.addEventListener('click', function(event) {
 
             let content = elem.cloneNode(true);
